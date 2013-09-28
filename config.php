018e779951f96d12ca11dda332f4819e4ab2599c
Jamie Gracie <k1hosting@gmail.com>
2013-09-28 13:59:59 +1200

    Adding basic config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$conf['debug'] = "1";
+	
+	$conf['db_hostname'] = "localhost";
+	$conf['db_username'] = "username";
+	$conf['db_password'] = "password";
+	$conf['db_name'] = "";
+
+	$conf['page_id'] = "";
+	$conf['facebook_app_id'] = "";
+	$conf['facebook_secret'] = "";
+	$conf['access_token'] = "";
+	
+?>
